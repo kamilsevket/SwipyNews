7d295ed3bfc4f87dabd8816438181c43a87646dd
Kamil Demirtaş <kamilsevket@hotmail.com>
2017-11-13 16:44:17 +0300

    Initial Commit

---
+++ DMSSwipeCards Example/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DMSSwipeCards Example
+//
+//  Created by Kamil on 13.11.2017.
+//  Copyright © 2017 Kamil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
